9aef2cdb0f9265c37ff663741004e7b0056b1407
tetyana <ptupob@mail.ru>
2016-06-02 21:44:49 +0300

    pifagor table

--- lesson_java/pifagor.js
+++ lesson_java/pifagor.js
@@ -1,7 +1,15 @@
-for (var i=1; i<=10;i++){
-	document.write("<tr>");
-	for (var j=1; j<=10;j++){
-	document.write("<td>"+i*j+"</td>");	
+
+   var mon=document.getElementById("main");
+   for (var i=1; i<=9;i++){
+var	row=document.createElement("div");
+	row.setAttribute("class","row");
+	
+	   for (var j=1; j<=9;j++){
+		   var prod=i*j;
+	   var cell=document.createElement("div");
+       cell.setAttribute("class","cell");
+       cell.innerHTML=prod;
+       row.appendChild(cell);	   
+	   }
+	 mon.appendChild(row);  
 	}
-	document.write("</tr>");
-}
